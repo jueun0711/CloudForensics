ea00c66859a95ce5ac934da89d9693ddb715b4e3
jueun0711 <103485966+jueun0711@users.noreply.github.com>
2022-07-18 11:22:14 +0900

    Create ZoomRecording.cs
    zoom cloud forensic

---
+++ code_refactoring/ZoomModel/ZoomRecording.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CloudForensics.ZoomModel
+{
+    public class ZoomRecording
+    {
+        public string Uuid { get; set; }
+
+        public long Id { get; set; }
+
+        public string AccountId { get; set; }
+
+        public string HostId { get; set; }
+
+        public string Topic { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public long Duration { get; set; }
+
+        public long FilesCount { get; set; }
+
+        public string ShareUrl { get; set; }
+
+        public long TotalSize { get; set; }
+
+        public int Index { get; set; }
+
+        public int TotalRecords { get; set; }
+
+        public override string ToString()
+        {
+            return "---------Recording information---------\n"
+           + $"Topic : {Topic}\n"
+           + $"User Id: {Id}\n"
+           + $"Host key : {HostId}\n"
+           + $"Account Id : {AccountId}\n"
+           + $"Uuid : {Uuid}\n"
+           + $"Start Time : {StartTime}\n"
+           + $"Duration : {Duration}\n"
+            + $"FIlesCount : {FilesCount}\n"
+            + $"Share Url: {ShareUrl}\n"
+            + $"Total Size : {TotalSize}\n\n";
+        }
+    }
+}
