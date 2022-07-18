d8918aae3c9e4d70ef922b0318482e8a23d43565
jueun0711 <103485966+jueun0711@users.noreply.github.com>
2022-07-18 11:26:07 +0900

    Update ZoomChatMem.cs

--- code_refactoring/ZoomModel/ZoomChatMem.cs
+++ code_refactoring/ZoomModel/ZoomChatMem.cs
@@ -5,13 +5,13 @@ namespace CloudForensics.ZoomModel
 {
     public class ZoomChatMem
     {
-		public string Id { get; set; }
+	public string Id { get; set; }
 
-		public string Email { get; set; }
+	public string Email { get; set; }
 
-		public string Name { get; set; }
+	public string Name { get; set; }
 
-		public ChatChannelRole Role { get; set; }
+	public ChatChannelRole Role { get; set; }
 
         public int Index { get; set; }
 
@@ -25,14 +25,6 @@ namespace CloudForensics.ZoomModel
                 + $"Email : {Email}\n"
                 + $"Id : {Id}\n"
                 + $"Role : {Role}\n\n";
-
-            /*Console.WriteLine($"--------Print Chat Channels Member Information-------");
-            Console.WriteLine($"Member Index / Total Member : {Index} / {TotalRecords}");
-            Console.WriteLine($"Name : {Name}");
-            Console.WriteLine($"Email : {Email}");
-            Console.WriteLine($"Id : {Id}");
-            Console.WriteLine($"Role : {Role}");
-            Console.WriteLine();*/
         }
     }
 }
