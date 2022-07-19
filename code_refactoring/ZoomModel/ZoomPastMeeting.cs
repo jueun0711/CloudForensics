1cc5246a674c31842a173c4b27a758c9ea13c496
jueun0711 <103485966+jueun0711@users.noreply.github.com>
2022-07-19 11:19:46 +0900

    Update ZoomPastMeeting.cs
    edit model

--- code_refactoring/ZoomModel/ZoomPastMeeting.cs
+++ code_refactoring/ZoomModel/ZoomPastMeeting.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZoomNet;
 using ZoomNet.Models;
 namespace CloudForensics.ZoomModel
 {
-    public class ZoomPastMeeting
+    public class ZoomPastMeeting        //https://github.com/Jericho/ZoomNet/blob/develop/Source/ZoomNet/Models/Meeting.cs
     {
         public string Uuid { get; set; }
 
@@ -19,17 +14,46 @@ namespace CloudForensics.ZoomModel
 
         public MeetingType Type { get; set; }
 
+        public MeetingStatus? Status { get; set; }
+
+        public string Agenda { get; set; }
+
         public DateTime CreatedOn { get; set; }
+
+        public string StartUrl { get; set; }
+
+        public string JoinUrl { get; set; }
+
+        public string Password { get; set; }
+
+        public string H323Password { get; set; }
+
+        public string PstnPassword { get; set; }
+
+        public MeetingSettings Settings { get; set; }
+
+        public int Index { get; set; }
+
+        public int TotalRecords { get; set; }
 
         public override string ToString()
         {
             return "---------Past Meeting information---------\n"
-           + $"User Id: {Id}\n"
-           + $"Host key : {HostId}\n"
-           + $"Topic : {Topic}\n"
-           + $"Created On : {CreatedOn}\n"
-           + $"Uuid : {Uuid}\n"
-           + $"Type : {Type}\n\n";
-        }
+                + $"Index / Total Meeting : {Index} / {TotalRecords}\n"
+                + $"Uuid : {Uuid}\n"
+                + $"User Id: {Id}\n"
+                + $"Host key : {HostId}\n"
+                + $"Topic : {Topic}\n"
+                + $"Type : {Type}\n";
+                //+ $"Status : {Status}\n"
+                //+ $"Agenda : {Agenda}\n"
+                //+ $"Created On : {CreatedOn}\n"
+                //+ $"StartUrl : {StartUrl}\n"
+                //+ $"JoinUrl : {JoinUrl}\n"
+                //+ $"Password : {Password}\n"
+                //+ $"H323Password : {H323Password}\n"
+                //+ $"PstnPassword : {PstnPassword}\n"
+                //+ $"Settings : {Settings}\n\n";
+    }
     }
 }
