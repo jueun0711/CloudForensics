f3e062b652d20a7cffb92edc704ccbbbcd0345a9
jueun0711 <103485966+jueun0711@users.noreply.github.com>
2022-07-21 09:01:20 +0900

    Update Program.cs
    add saving file func

--- code_refactoring/Program.cs
+++ code_refactoring/Program.cs
@@ -12,8 +12,10 @@ namespace CloudForensics
         private const string id = "forensics_marketing@hancom.com";
 
         //variable
-        private const string accessToken = "eyJhbGciOiJIUzUxMiIsInYiOiIyLjAiLCJraWQiOiJiZDdkYzUwNi01NmJiLTRkNGEtYjdiZS1kYmJjZWY3M2E2NTEifQ.eyJ2ZXIiOjcsImF1aWQiOiI1Yzg0NjljMmNmNTRhMDNjZTc4NTQxOGNhOGVmYTFjNCIsImNvZGUiOiJZMG5sSDVQMU5QX2tWNXZjZ1RFUmdpNlFzdUtobkhzX3ciLCJpc3MiOiJ6bTpjaWQ6Mk9vNEFvcXRSaXFNdTZiSjlHSWtRdyIsImdubyI6MCwidHlwZSI6MCwidGlkIjowLCJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJrVjV2Y2dURVJnaTZRc3VLaG5Ic193IiwibmJmIjoxNjU4Mjc4MDk2LCJleHAiOjE2NTgyODE2OTYsImlhdCI6MTY1ODI3ODA5NiwiYWlkIjoiWE84azhod01RZWFGYU5IX29XMjVVQSIsImp0aSI6IjgyZmE0ZjIzLTAxM2UtNGRjMC05MGZlLTNjOGEyNGVjMDE2ZiJ9._PbprLwt2B5ePxRWGt3ratAkTxi0XiCjY3JaQCh-xeG4658CdQY8mzGhS4CKU2sFskVviWlO6YbP4C4ocO6Bog";
-        private const string refreshToken = "eyJhbGciOiJIUzUxMiIsInYiOiIyLjAiLCJraWQiOiI4ZGE2YjYyMC00M2UwLTQwODItOTA1Ny1jMDE5N2Q0MTZiMDIifQ.eyJ2ZXIiOjcsImF1aWQiOiI1Yzg0NjljMmNmNTRhMDNjZTc4NTQxOGNhOGVmYTFjNCIsImNvZGUiOiJZMG5sSDVQMU5QX2tWNXZjZ1RFUmdpNlFzdUtobkhzX3ciLCJpc3MiOiJ6bTpjaWQ6Mk9vNEFvcXRSaXFNdTZiSjlHSWtRdyIsImdubyI6MCwidHlwZSI6MSwidGlkIjowLCJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJrVjV2Y2dURVJnaTZRc3VLaG5Ic193IiwibmJmIjoxNjU4Mjc4MDk2LCJleHAiOjIxMzEzMTgwOTYsImlhdCI6MTY1ODI3ODA5NiwiYWlkIjoiWE84azhod01RZWFGYU5IX29XMjVVQSIsImp0aSI6IjhmNjUzYTQwLTNhMzctNDVhMi1iYjE0LWJlYTRjZjMwYTI0NiJ9.arSp6DClaeDFbs_KnDRd32xLjBbtHFb0wKkp7Kq7CGlRVwgOVsCkz0mYRd4kZk6lhW8wlkZGqM1YiyOPbIeeIA";
+        private const string accessToken = "eyJhbGciOiJIUzUxMiIsInYiOiIyLjAiLCJraWQiOiJiMTNiYmQ4Mi05YmUyLTQ2NzktOWJkYy0zYWRlZWZjMDhiYTIifQ.eyJ2ZXIiOjcsImF1aWQiOiI1Yzg0NjljMmNmNTRhMDNjZTc4NTQxOGNhOGVmYTFjNCIsImNvZGUiOiJ5bExxeTV1VEZlX2tWNXZjZ1RFUmdpNlFzdUtobkhzX3ciLCJpc3MiOiJ6bTpjaWQ6Mk9vNEFvcXRSaXFNdTZiSjlHSWtRdyIsImdubyI6MCwidHlwZSI6MCwidGlkIjowLCJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJrVjV2Y2dURVJnaTZRc3VLaG5Ic193IiwibmJmIjoxNjU4MzU5NzU1LCJleHAiOjE2NTgzNjMzNTUsImlhdCI6MTY1ODM1OTc1NSwiYWlkIjoiWE84azhod01RZWFGYU5IX29XMjVVQSIsImp0aSI6ImM1MTIyMzE3LWQ2ZGUtNDZjMS1iMDY3LTFjYzAxOTVkMmQ4MyJ9.wFFgL6Q0UDEBdggQJseSoR4dWQSDuCvxS6HPFPuhnhFgT8zIMUhKBWtrbhniQQOe6L5M6ruDGxMtfu3XsMrkQg";
+        private const string refreshToken = "eyJhbGciOiJIUzUxMiIsInYiOiIyLjAiLCJraWQiOiI3YzMyNmYyOC01NzExLTQxMjQtYTgxOS00OTRmNmZkNjI0YTEifQ.eyJ2ZXIiOjcsImF1aWQiOiI1Yzg0NjljMmNmNTRhMDNjZTc4NTQxOGNhOGVmYTFjNCIsImNvZGUiOiJ5bExxeTV1VEZlX2tWNXZjZ1RFUmdpNlFzdUtobkhzX3ciLCJpc3MiOiJ6bTpjaWQ6Mk9vNEFvcXRSaXFNdTZiSjlHSWtRdyIsImdubyI6MCwidHlwZSI6MSwidGlkIjowLCJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiJrVjV2Y2dURVJnaTZRc3VLaG5Ic193IiwibmJmIjoxNjU4MzU5NzU1LCJleHAiOjIxMzEzOTk3NTUsImlhdCI6MTY1ODM1OTc1NSwiYWlkIjoiWE84azhod01RZWFGYU5IX29XMjVVQSIsImp0aSI6Ijk2NDFhZTYyLTZjYmEtNGIzZS1hZjZiLWVjMjAwYWJhYmRkNiJ9.jjHIKsE47Nq44N9hf-Iv4US4nvUVdQEpcF-0Czu63F0Evaug7Lr9Y2dD-QroC3DALuQ7iZUL7etHkrQEqedwsA";
+
+        private const string downloadPath = "C:/Users/USER/Downloads";
 
         static void Main(string[] args)
         {
@@ -36,7 +38,11 @@ namespace CloudForensics
                 for (int j = 0; j < recordings[i].RecordingFiles.Length; j++)
                 {
                     Console.WriteLine($"{recordings[i].RecordingFiles[j]}");
-                    System.Diagnostics.Process.Start(recordings[i].RecordingFiles[j].DownloadUrl);
+
+                    bool success = zoom.DownloadRecordings(recordings[i], downloadPath).Result;
+
+                    if(success) Console.WriteLine($"file download : success\n");
+                    else Console.WriteLine($"file download : fail\n");
                 }
             }
             /*for (int i = 0; i < chatList.Count; i++)
@@ -52,6 +58,7 @@ namespace CloudForensics
                 }
             }*/
 
+            Console.WriteLine("====== End ======");
             Console.ReadKey();
         }
     }
